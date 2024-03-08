09accc18f40bba47e85ec40c650825358a28e2fa
spfarooq <160659062+spfarooq@users.noreply.github.com>
2024-03-08 23:50:47 +0530

    Update variable.tf

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,25 +1,19 @@
 variable "ins_type" {
     type = string
-    default = "t2.micro"
 }
 variable "ec2_key" {
-    type = string
-    default = "linux"
+    type = string  
 }
 
 variable "cidr_sub1" {
-    type = string
-    default = "10.0.1.0/24"
+    type = string   
 }
 variable "cidr_sub2" {
     type = string
-    default = "10.0.2.0/24"
 }
 variable "ec2_name1" {
     type = string
-    default = "linux-pub1"
 }
 variable "ami_id" {
     type = string
-    default = ""
 }
