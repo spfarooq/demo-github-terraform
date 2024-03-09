c395ee4d2cdd645a88ac8dca6120ad77c05fbadf
spfarooq <160659062+spfarooq@users.noreply.github.com>
2024-03-09 22:46:09 +0530

    Update variable.tf

--- s3/variable.tf
+++ s3/variable.tf
@@ -1,1 +1,13 @@
+# variables.tf
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket"
+}
+
+variable "acl" {
+  type        = string
+  description = "The canned ACL (Access Control List) for the S3 bucket"
+  default     = "private"
+}
 
