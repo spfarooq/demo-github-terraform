3c62a9c2535aaf045129f1c7aaf50a597f0f8f73
spfarooq <160659062+spfarooq@users.noreply.github.com>
2024-03-09 22:45:06 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,1 +1,15 @@
+# main.tf
 
+provider "aws" {
+  region = "us-east-1"  # Specify your desired AWS region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = "Production"
+  }
+}
